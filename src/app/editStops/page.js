58a1d33ff7554d97cf23e9ee50ae889e3c206fe0
webcs494 <webcs494@outlook.com>
2024-03-16 13:26:17 -0700

    List of stops

---
+++ src/app/editStops/page.js
@@ -0,0 +1,46 @@
+"use client"
+
+import { useState, useEffect } from "react";
+import styles from "../page.module.css";
+
+export default function EditStops() {
+    return(
+        <div className={styles.main}>
+            <h1>Edit Favorite Stops</h1>
+            <Stops />
+        </div>
+    )
+}
+
+export function Stops() {
+    const [stops, setStops] = useState([])
+    const [id, setID] = useState([])
+
+    useEffect(() => {
+        async function fetchStops() {
+            const stops_response = await fetch("https://corvallisb.us/api/static")
+            const stops_json = await stops_response.json()
+            setStops(stops_json.stops)
+
+        } fetchStops()
+    }, [])
+
+    // console.log("Stops: ", stops)
+    // Get stop ID from JSON
+    useEffect(() => {
+        const ids = []
+        for (let id in stops) {
+            ids[ids.length] = id
+        }
+        setID(ids)
+    }, [stops])
+    
+    // console.log(id)
+    return(
+        <ul>
+            {id.map((stop, index) => (
+                <li>{stops[id[index]].name}</li>
+            ))}
+        </ul> 
+    )
+}

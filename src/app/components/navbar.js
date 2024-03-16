6daf25b30cd299f0e64f02a157855cbe54dbc132
webcs494 <webcs494@outlook.com>
2024-03-16 11:03:08 -0700

    Changed name

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,7 +6,7 @@ export function Navbar() {
       <ul>
         <li><Link href="/">Home</Link></li>
         <li><Link href="/viewRoute">Route Info</Link></li>
-        <li><Link href="/weather">Weather</Link></li>
+        <li><Link href="/weather">Weather In Corvallis</Link></li>
       </ul>
     </nav>
   )

40b3e13ca93834d3471ac11d40a78a324d78137e
webcs494 <webcs494@outlook.com>
2024-03-16 11:04:29 -0700

    Home page in nav

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -4,7 +4,7 @@ export function Navbar() {
   return (
     <nav>
       <ul>
-        <li><Link href="/">Home</Link></li>
+        <li><Link href="/">Home Page</Link></li>
         <li><Link href="/viewRoute">Route Info</Link></li>
         <li><Link href="/weather">Weather In Corvallis</Link></li>
       </ul>

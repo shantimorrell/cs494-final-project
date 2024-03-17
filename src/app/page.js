d9eca42261b54809bfdcb358e1ee349a5d8fc56c
emmakoehmstedt <koehmste@oregonstate.edu>
2024-03-17 09:25:10 -0700

    styling for home page

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        <p>Welcome to Corvallis Bus Routes!</p>
       </div>
     </main>
   );
